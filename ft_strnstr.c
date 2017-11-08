2fe9e29f812549715df2545043921f59ceb7e31a
Clementine PIRLOT <cpirlot@e3r12p14.42.fr>
2017-11-08 15:38:31 +0100

    Fix error in strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 09:21:18 by cpirlot           #+#    #+#             */
-/*   Updated: 2017/11/08 14:18:52 by cpirlot          ###   ########.fr       */
+/*   Updated: 2017/11/08 15:38:12 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
 	size_t	i;
 	size_t	j;
+	size_t	len2;
 
-	if (ft_strlen(s1) == 0)
+	if (!s1)
 		return (NULL);
+	if (!s2 || !*s2)
+		return ((char *)s1);
 	i = 0;
 	j = 0;
-	if (s2[j] == '\0')
-		return ((char *)s1);
-	while (s1[i] != '\0' && len > 0)
+	len2 = len;
+	while (s1[i] != '\0' && i <= len)
 	{
-		while (len > 0 && s1[i] == s2[j])
+		while (i <= len && s1[i] == s2[j])
 		{
 			i++;
 			j++;
@@ -34,7 +36,7 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 		}
 		j = 0;
 		i++;
-		len--;
+		len2--;
 	}
 	return (NULL);
 }
