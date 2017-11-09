f187116a4c192e461d412facceb9f313b41e884d
Clementine PIRLOT <cpirlot@e3r12p14.42.fr>
2017-11-09 14:35:20 +0100

    fix errors in strnequ

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 09:19:43 by cpirlot           #+#    #+#             */
-/*   Updated: 2017/11/08 13:48:25 by cpirlot          ###   ########.fr       */
+/*   Updated: 2017/11/09 14:33:43 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,5 @@
 
 int	ft_strnequ(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
-
-	if (!s1)
-		return (0);
-	if (!s2 || n == 0)
-		return (1);
-	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n - 1)
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	return (1);
+	return (!ft_strncmp(s1, s2, n));
 }
