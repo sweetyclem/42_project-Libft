78c62ad896de50c3cbd9b088c2a928d3d709952a
Clementine PIRLOT <cpirlot@e3r12p14.42.fr>
2017-11-09 15:22:12 +0100

    fix strnequ in case of null

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 09:19:43 by cpirlot           #+#    #+#             */
-/*   Updated: 2017/11/09 14:33:43 by cpirlot          ###   ########.fr       */
+/*   Updated: 2017/11/09 15:17:55 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 int	ft_strnequ(const char *s1, const char *s2, size_t n)
 {
-	return (!ft_strncmp(s1, s2, n));
+	if (!s1 || !s2)
+		return (0);
+	if (ft_strncmp(s1, s2, n) == 0)
+		return (1);
+	return (0);
 }
