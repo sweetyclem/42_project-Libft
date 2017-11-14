9112abaf1413d44efd71ee54b7332e8c00ed9505
Clementine PIRLOT <cpirlot@e1r12p14.42.fr>
2017-11-14 08:56:40 +0100

    simplify strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 08:45:08 by cpirlot           #+#    #+#             */
-/*   Updated: 2017/11/08 08:45:33 by cpirlot          ###   ########.fr       */
+/*   Updated: 2017/11/14 08:04:56 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	res = ft_strlen(dest) + ft_strlen(src);
 	while (dest[i] != 0 && i < size)
 		i++;
-	if ((size - i) == 0)
+	if (i == size)
 		return (i + ft_strlen(src));
 	while (i != (size - 1) && src[j] != 0)
 	{
@@ -31,6 +31,6 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 		i++;
 		j++;
 	}
-	dest[i] = 0;
+	dest[i] = '\0';
 	return (res);
 }
